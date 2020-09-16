15ea6240efecd86e6a85126851b732980728e29f
Shital Giri <shitalngiri@gmail.com>
2020-09-15 20:40:09 -0400

    Added first event listener

--- scripts/script.js
+++ scripts/script.js
@@ -6,23 +6,32 @@ const moviesApp = {};
 
 
 // create a function that gets monkey artwork from Rijks museum
-moviesApp.getMovies = function () {
+moviesApp.getMovies = function (query) {
   $.ajax({
     url: "https://api.themoviedb.org/3/search/movie",
     method: "GET",
     dataType: "json",
     data: {
       api_key: '4f9560632152b0224a1ed3e7562039b6',
-      query: "spiderman",
+      query: query,
     },
   }).then((res) => {
     console.log(res);
   });
 };
 
-
+moviesApp.userInput = function() {
+$('form').on('submit', function(event) {
+    // console.log(" Lets's submit!");
+    event.preventDefault();
+    const userSearch = $('input').val();
+    console.log(userSearch);
+    // moviesApp.getMovies(userSearch);
+})
+}
 moviesApp.init = function (){
     moviesApp.getMovies();
+    moviesApp.userInput();
 };
 
 
