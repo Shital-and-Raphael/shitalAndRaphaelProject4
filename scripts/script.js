14b08e5b3fe941cfd31d03de735c98c85907851d
Shital Giri <shitalngiri@gmail.com>
2020-09-16 21:18:21 -0400

    Added showing movie trailer links.

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,7 @@
-console.log("ready to code !");
 
 // creating our app object
 const moviesApp = {};
 
-// create a function that gets monkey artwork from Rijks museum
 moviesApp.getMovies = function (query) {
   $.ajax({
     url: "https://api.themoviedb.org/3/search/movie",
@@ -14,9 +12,19 @@ moviesApp.getMovies = function (query) {
       query: query,
     },
   }).then((res) => {
-    // console.log(res.results);
     $("#results").empty();
-    moviesApp.displayMovies(res.results);
+    res.results.forEach((movie) => {
+      $.ajax({
+        url: `https://api.themoviedb.org/3/movie/${movie.id}/videos`,
+        method: "GET",
+        dataType: "JSON",
+        data: {
+          api_key: "4f9560632152b0224a1ed3e7562039b6",
+        },
+      }).then((trailers) => {
+        moviesApp.addMovieDisplay(movie, trailers);
+      });
+    });
   });
 };
 
@@ -31,32 +39,56 @@ moviesApp.userInput = function () {
   });
 };
 
-moviesApp.displayMovies = function (movie) {
-  movie.forEach((piece) => {
-    console.log(piece);
-    // const title = $("<h2>").text(piece.title);
-    // const overview = $("<p>").text(piece.overview);
-    // const image = $("<img>").attr({
-    //   src: `http://image.tmdb.org/t/p/w300_and_h450_bestv2/${piece.poster_path}`,
-    //   alt: piece.title,
-    // });
-    // $("#results").append(
-    //   `<div class="test"><img src='http://image.tmdb.org/t/p/w300_and_h450_bestv2/${piece.poster_path}' alt=""></div>`
-    // );
-    $("#results").append(`
+moviesApp.addMovieDisplay = function (movie, trailers) {
+  console.log(movie);
+  console.log(trailers);
+  let videoHtml = "";
+  trailers.results.forEach((trailer) => {
+      //https://www.freecodecamp.org/news/how-to-use-html-to-open-link-in-new-tab/
+    videoHtml += `<a href="https://www.youtube.com/watch?v=${trailer.key}" target="_blank" rel="noopener noreferrer">${trailer.name}</a>`;
+  });
+
+  $("#results").append(`
     <div class="movie-container">
       <div class="image-container">
-        <img src='http://image.tmdb.org/t/p/w300_and_h450_bestv2/${piece.poster_path}' alt=""></div>
+        <img src='http://image.tmdb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}' alt=""></div>
       <div class='info'>
-        <h2>${piece.title}</h2>
-        <p class='release-date'>Release date: ${piece.release_date}</p>
-        <p class='overview'>${piece.overview}</p>
-        <p>Popularity: ${piece.popularity}</p>
+        <h2>${movie.title}</h2>
+        <p class='release-date'>Release date: ${movie.release_date}</p>
+        <p>Ratings: ${movie.vote_average}</p>
+        <p class='overview'>${movie.overview}</p>
+        <p>${videoHtml}</p>
       <div>
     </div>
     `);
-  });
 };
+
+// moviesApp.displayMovies = function (movie) {
+//   movie.forEach((piece) => {
+//     console.log(piece);
+//     // const title = $("<h2>").text(piece.title);
+//     // const overview = $("<p>").text(piece.overview);
+//     // const image = $("<img>").attr({
+//     //   src: `http://image.tmdb.org/t/p/w300_and_h450_bestv2/${piece.poster_path}`,
+//     //   alt: piece.title,
+//     // });
+//     // $("#results").append(
+//     //   `<div class="test"><img src='http://image.tmdb.org/t/p/w300_and_h450_bestv2/${piece.poster_path}' alt=""></div>`
+//     // );
+//     $("#results").append(`
+//     <div class="movie-container">
+//       <div class="image-container">
+//         <img src='http://image.tmdb.org/t/p/w300_and_h450_bestv2/${piece.poster_path}' alt=""></div>
+//       <div class='info'>
+//         <h2>${piece.title}</h2>
+//         <p class='release-date'>Release date: ${piece.release_date}</p>
+//         <p>Ratings: ${piece.vote_average}</p>
+//         <p class='overview'>${piece.overview}</p>
+//       <div>
+//     </div>
+//     `);
+//   });
+// };
 
 moviesApp.init = function () {
   moviesApp.userInput();
